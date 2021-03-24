f5aed4449fbd78212d10d03c21e051f76d021326
elmnik <elmnik36876@gmail.com>
2021-03-24 13:13:31 -0600

    commit

---
+++ cypress/integration/examples/authFunct.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer'); //declaration for the puppeteer function method
+
+exports.getAadToken = async function getAadToken(userObject){
+    const {email, password, root} =userObject;
+    const creds = await puppeteer
+    .launch({headless: true})
+    .then(async browser => {
+        try{
+            let page = await browser.newPage();
+            //login page
+            await page.goto(root);
+            await page.click();
+            await page.waitfor(5000);
+
+            const pages = await browser.pages();
+            const popUp = pages.pop();
+            //Azure login 
+            await popUp.click('input[type=email]');
+            await popUp.type('input[type=email]', email,{
+                delay: 50
+            });
+            await popUp.waitfor(5000);
+            await popUp.click('input[type=submit]');
+            await popUp.waitfor(5000);
+            await popUp.click('input[type="password"]');
+            await popUp.waitfor(5000);
+            await popUp.type('input[type="password"]', password,{
+                delay:50
+            });
+            await popUp.waitfor(5000);
+            await popUp.click('input[type=submit]');
+
+            const confirm = await popUp.content();
+
+            if(confirm.includes('sign in to your account')) {
+                await popUp.waitfor(5000);
+                await popUp.click('input[type=submit]');
+            }
+
+        }
+
+    
+    })
+}
