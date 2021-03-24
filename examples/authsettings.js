8c2b4493876dc1b6851fc381d9c04eadda2a8332
elmnik <elmnik36876@gmail.com>
2021-03-24 11:38:12 -0600

    commit

--- examples/authsettings.js
+++ examples/authsettings.js
@@ -1,5 +1,5 @@
 {
-    "authority": "https://login.microsoftonline.com/your-add-tenant-id",
+    "authority":"https://login.microsoftonline.com/your-add-tenant-id",
     "clientId":  "cfc2879bd-db1e-4072-83c9-a95fe61223d3",
     "clientSecret": "7ac59ed3-74a0-4c0a-9e96-65b3a9d1144c",
     "apiscopes": ["api://api-client-id/Data.Read"],
